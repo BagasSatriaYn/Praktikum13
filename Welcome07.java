33d4ab98faa29e5fc5c45446b168db357627c1ff
BagasSatriaYn <duhjvdedhvhjufahfy@gmail.com>
2023-11-29 11:45:07 +0700

    Percobaan 1

---
+++ Welcome07.java
@@ -0,0 +1,12 @@
+package Praktikum13;
+
+public class Welcome07 {
+    public static void displayWelcomMessage(){
+        System.out.println("Selamat datang di Politeknik Negeri Malang");
+        
+    }
+    public static void main(String[] args) {
+        displayWelcomMessage();
+    }
+    
+}
